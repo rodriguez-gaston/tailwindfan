f5465e5d356f725c178da6b16682423836254d12
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-07-27 20:21:56 -0300

    Remove color

--- pages/blocks/index.tsx
+++ pages/blocks/index.tsx
@@ -3,11 +3,11 @@ import { NextPage } from 'next';
 
 const BlocksPage: NextPage = () => {
   return (
-    <section className="bg-gradient-to-b from-white to-cyan-50 py-20 px-8">
-      <div className="max-w-7xl mx-auto">
+    <section className="py-10 lg:pt-20 px-8">
+      <section className="max-w-7xl mx-auto">
         <p>Esta es la Component Page</p>
         <Block />
-      </div>
+      </section>
     </section>
   );
 };
