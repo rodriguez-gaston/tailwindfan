4c01c9d6a651a2a40f0c328ce2e311ecf1df25a1
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-07-27 00:07:19 -0300

    Change maxWidth

--- lib/breakpoints.ts
+++ lib/breakpoints.ts
@@ -3,22 +3,22 @@ import { Breakpoint } from '@/interface/breakpoint';
 export const allBreakpoints: Array<Breakpoint> = [
   {
     name: 'Mobile',
-    maxWidth: 'max-w-[340px]',
+    maxWidth: '340px',
   },
   {
     name: 'Small',
-    maxWidth: 'max-w-[640px]',
+    maxWidth: '640px',
   },
   {
     name: 'Medium',
-    maxWidth: 'max-w-[768px]',
+    maxWidth: '768px',
   },
   {
     name: 'Large',
-    maxWidth: 'max-w-[1024px]',
+    maxWidth: '1024px',
   },
   {
     name: 'Full',
-    maxWidth: 'max-w-full',
+    maxWidth: '100%',
   },
 ];
