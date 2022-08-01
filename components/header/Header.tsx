9ca62db4d9fa34bec9028cd1eda6b1cf7d3e7610
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-08-01 18:51:43 -0300

    Change layout and responsive on navbar

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -4,8 +4,8 @@ import Logo from '@/components/header/Logo';
 
 const Header = () => {
   return (
-    <header className="h-16 border-b border-gray-300 flex items-center px-8">
-      <section className="max-w-7xl flex items-center justify-between w-full mx-auto">
+    <header className="border-b border-gray-300 flex items-center px-8 py-2">
+      <section className="max-w-7xl flex md:flex-row flex-col items-center justify-between w-full mx-auto">
         <Link href="/">
           <a>
             <section className="text-blue-500 flex gap-2 items-center">
@@ -16,13 +16,13 @@ const Header = () => {
             </section>
           </a>
         </Link>
-        <nav className="text-gray-800 font-semibold items-center justify-end gap-8 md:flex hidden">
+        <nav className="text-gray-800 font-semibold text-sm items-center justify-end gap-4 flex">
           <Link href="/blocks">Blocks</Link>
           <Link href="/templates">Templates</Link>
           <Link href="/colors">Colors</Link>
           <Link href="/learn">Learn</Link>
           <Link href="/contact">
-            <a className="bg-cyan-700 hover:bg-cyan-900 transition duration-300 text-white rounded-lg px-4 py-3">
+            <a className="bg-cyan-700 hover:bg-cyan-900 transition duration-300 text-white rounded-lg px-2 py-3">
               Custom UI
             </a>
           </Link>
