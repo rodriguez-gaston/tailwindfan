8339ed55a4fe1be316e6da07568e4be5117258ad
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-08-01 18:51:20 -0300

    Change display on footer

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -9,58 +9,62 @@ import Medium from '@/components/icons/Medium';
 const Footer = () => {
   return (
     <footer className="bg-gray-100 p-8">
-      <section className="max-w-7xl w-full mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 justify-start items-start">
-        <section className="space-y-2 col-span-1">
-          <Link href="/">
-            <a>
-              <section className="text-blue-500 flex gap-2 items-center">
-                <Logo />
-                <span className="text-gray-800 font-bold text-lg">
-                  TailwindFan
-                </span>
-              </section>
-            </a>
-          </Link>
-          <p className="text-sm text-gray-600">Tailwind blocks ready to use</p>
-          <section className="flex gap-2">
-            <a
-              href="https://instagram.com/codewithgaston"
-              target="_blank"
-              rel="noreferrer"
-              className="bg-gray-500 p-1 text-white rounded-full hover:bg-gray-700"
-            >
-              <Instagram />
-            </a>
-            <a
-              href="https://www.youtube.com/channel/UC8mpmC8Zyd5ga2AC2pkQ2tA?sub_confirmation=1"
-              target="_blank"
-              rel="noreferrer"
-              className="bg-gray-500 p-1 text-white rounded-full hover:bg-gray-700"
-            >
-              <Youtube />
-            </a>
-            <a
-              href="https://github.com/rodriguez-gaston"
-              target="_blank"
-              rel="noreferrer"
-              className="bg-gray-500 p-1 text-white rounded-full hover:bg-gray-700"
-            >
-              <Github />
-            </a>
-            <a
-              href="https://medium.com/@codewithgaston"
-              target="_blank"
-              rel="noreferrer"
-              className="bg-gray-500 p-1 text-white rounded-full hover:bg-gray-700"
-            >
-              <Medium />
-            </a>
+      <section className="max-w-7xl w-full mx-auto space-y-4">
+        <section className="flex flex-col sm:flex-row gap-4 justify-between items-start">
+          <section className="space-y-2 col-span-1">
+            <Link href="/">
+              <a>
+                <section className="text-blue-500 flex gap-2 items-center">
+                  <Logo />
+                  <span className="text-gray-800 font-bold text-lg">
+                    TailwindFan
+                  </span>
+                </section>
+              </a>
+            </Link>
+            <p className="text-sm text-gray-600">
+              Tailwind blocks ready to use
+            </p>
+            <section className="flex gap-2">
+              <a
+                href="https://instagram.com/codewithgaston"
+                target="_blank"
+                rel="noreferrer"
+                className="bg-gray-500 p-1 text-white rounded-full hover:bg-gray-700"
+              >
+                <Instagram />
+              </a>
+              <a
+                href="https://www.youtube.com/channel/UC8mpmC8Zyd5ga2AC2pkQ2tA?sub_confirmation=1"
+                target="_blank"
+                rel="noreferrer"
+                className="bg-gray-500 p-1 text-white rounded-full hover:bg-gray-700"
+              >
+                <Youtube />
+              </a>
+              <a
+                href="https://github.com/rodriguez-gaston"
+                target="_blank"
+                rel="noreferrer"
+                className="bg-gray-500 p-1 text-white rounded-full hover:bg-gray-700"
+              >
+                <Github />
+              </a>
+              <a
+                href="https://medium.com/@codewithgaston"
+                target="_blank"
+                rel="noreferrer"
+                className="bg-gray-500 p-1 text-white rounded-full hover:bg-gray-700"
+              >
+                <Medium />
+              </a>
+            </section>
           </section>
-        </section>
-        <section className="col-span-1 flex flex-col text-gray-800 text-sm font-medium">
-          <Link href="/blocks">Blocks</Link>
-          <Link href="/colors">Colors</Link>
-          <Link href="/learn">Learn</Link>
+          <section className="col-span-1 flex gap-8 text-gray-800 text-sm font-medium">
+            <Link href="/blocks">Blocks</Link>
+            <Link href="/colors">Colors</Link>
+            <Link href="/learn">Learn</Link>
+          </section>
         </section>
         <section className="col-span-1 lg:col-span-2">
           <p className="text-xs text-gray-500">by @codewithgaston</p>
