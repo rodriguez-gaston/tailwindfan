84cfb39f529986ad32f84637211fed0b6dce12c2
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-08-02 19:05:27 -0300

    Remove CTA

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -5,7 +5,7 @@ import Logo from '@/components/header/Logo';
 const Header = () => {
   return (
     <header className="border-b border-gray-300 flex items-center px-8 py-2">
-      <section className="max-w-7xl flex md:flex-row flex-col items-center justify-between w-full mx-auto">
+      <section className="max-w-7xl flex md:flex-row flex-col gap-2 items-center justify-between w-full mx-auto">
         <Link href="/">
           <a>
             <section className="text-blue-500 flex gap-2 items-center">
@@ -21,11 +21,6 @@ const Header = () => {
           <Link href="/templates">Templates</Link>
           <Link href="/colors">Colors</Link>
           <Link href="/learn">Learn</Link>
-          <Link href="/contact">
-            <a className="bg-cyan-700 hover:bg-cyan-900 transition duration-300 text-white rounded-lg px-2 py-3">
-              Custom UI
-            </a>
-          </Link>
         </nav>
       </section>
     </header>
