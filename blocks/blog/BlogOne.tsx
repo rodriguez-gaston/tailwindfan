8d0e98aa8c3f65eadca14b1748eca64e090e829e
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-08-12 20:58:28 -0300

    Blocks folder structure

---
+++ blocks/blog/BlogOne.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BlogOne = () => {
+  return (
+    <div>
+      <p>Test</p>
+    </div>
+  );
+};
+
+export default BlogOne;
---
+++ blocks/card/CardOne.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CardOne = () => {
+  return (
+    <div>
+      <p>Test</p>
+    </div>
+  );
+};
+
+export default CardOne;
