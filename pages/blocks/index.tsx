a7573c9e47169eeb6b9fe1a40e4196515d7a1123
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-08-12 20:59:49 -0300

    Fix blocks pag3

--- pages/blocks/index.tsx
+++ pages/blocks/index.tsx
@@ -5,19 +5,21 @@ import { NextPage } from 'next';
 const BlocksPage: NextPage = () => {
   return (
     <section className="py-10 lg:pt-20 px-8">
-      <section className="max-w-7xl mx-auto">
-        <p>Esta es la Component Page</p>
-        {blocksList.map((block) => {
-          return (
-            <Block
-              id={block.id}
-              title={block.title}
-              category={block.category}
-              block={block.block}
-              key={block.id}
-            />
-          );
-        })}
+      <section className="max-w-7xl mx-auto space-y-12">
+        <h1 className="text-5xl font-bold text-gray-800">Blocks page</h1>
+        <section className="space-y-8">
+          {blocksList.map((block) => {
+            return (
+              <Block
+                id={block.id}
+                title={block.title}
+                category={block.category}
+                block={block.block}
+                key={block.id}
+              />
+            );
+          })}
+        </section>
       </section>
     </section>
   );
