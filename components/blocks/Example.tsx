4ae0a344d556a8534b53d0fee6ad1de0aa647c36
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-08-12 18:56:33 -0300

    Test fetch block

---
+++ components/blocks/Example.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Example = () => {
+  return (
+    <section className="bg-blue-500 p-4 flex justify-between">
+      <p className="text-white">Section</p>
+      <p className="text-white">Section</p>
+    </section>
+  );
+};
+
+export default Example;
