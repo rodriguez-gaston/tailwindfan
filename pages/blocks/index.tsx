30b119577b4c627aed2233f434e0eb312404e590
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-08-12 18:56:08 -0300

    Map blocks from list

--- pages/blocks/index.tsx
+++ pages/blocks/index.tsx
@@ -1,4 +1,5 @@
 import Block from '@/components/blocks/Block';
+import { blocksList } from '@/lib/Components';
 import { NextPage } from 'next';
 
 const BlocksPage: NextPage = () => {
@@ -6,7 +7,17 @@ const BlocksPage: NextPage = () => {
     <section className="py-10 lg:pt-20 px-8">
       <section className="max-w-7xl mx-auto">
         <p>Esta es la Component Page</p>
-        <Block />
+        {blocksList.map((block) => {
+          return (
+            <Block
+              id={block.id}
+              title={block.title}
+              category={block.category}
+              block={block.block}
+              key={block.id}
+            />
+          );
+        })}
       </section>
     </section>
   );
