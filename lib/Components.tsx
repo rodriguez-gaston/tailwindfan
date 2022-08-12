496b778d86faa81862d155ee9f92b7d3eb6fb0c8
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-08-12 18:52:19 -0300

    Component list to map blocks

---
+++ lib/Components.tsx
@@ -0,0 +1,10 @@
+import Example from '@/components/blocks/Example';
+
+export const blocksList = [
+  {
+    id: 'example',
+    category: 'example',
+    title: 'Simple Alert',
+    block: <Example />,
+  },
+];
