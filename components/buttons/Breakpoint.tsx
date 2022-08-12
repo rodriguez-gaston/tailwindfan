34b5a3daaf3007fa883b83b93475425394cb654d
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-08-12 20:59:00 -0300

    Fix padding on button

--- components/buttons/Breakpoint.tsx
+++ components/buttons/Breakpoint.tsx
@@ -18,7 +18,7 @@ const Breakpoint = ({ name, onClick }: BreakpointProps) => {
       />
       <label
         htmlFor={name}
-        className="flex items-center text-sm p-2 rounded-lg border-2 border-blue-800 hover:bg-blue-800 hover:text-white peer-checked:bg-blue-800 peer-checked:text-white cursor-pointer transition"
+        className="flex items-center text-sm px-2 py-2.5 rounded-lg border-2 border-blue-800 hover:bg-blue-800 hover:text-white peer-checked:bg-blue-800 peer-checked:text-white cursor-pointer transition"
       >
         <p>{name}</p>
       </label>
