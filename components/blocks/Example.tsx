6e228f60ac28dec49049e3fc271848324507f4fa
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-08-12 23:31:13 -0300

    Remove props

--- components/blocks/Example.tsx
+++ components/blocks/Example.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Example = () => {
   return (
-    <section className="bg-blue-500 p-4 flex justify-between">
+    <section className="bg-blue-600 p-4 flex justify-between">
       <p className="text-white">Section</p>
       <p className="text-white">Section</p>
     </section>
