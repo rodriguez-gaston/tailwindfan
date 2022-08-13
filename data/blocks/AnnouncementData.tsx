383e22d6b64499cd81e3e6137a5baea3f83738e4
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-08-12 23:35:26 -0300

    New categories data

---
+++ data/blocks/AnnouncementData.tsx
@@ -0,0 +1,25 @@
+const AnnouncementData = {
+  title: '',
+  blocks: [
+    {
+      id: 'example',
+      category: 'example',
+      title: 'Simple Alert',
+      block: '',
+    },
+    {
+      id: '1',
+      category: 'announcement',
+      title: 'Simple Announcement',
+      block: '',
+    },
+    {
+      id: '1',
+      category: 'avatar',
+      title: 'Simple Avatar',
+      block: '',
+    },
+  ],
+};
+
+export default AnnouncementData;
