81e0337555199065ad532cbd8be957fa217b9a35
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-08-19 17:36:03 -0300

    Add icon

--- components/buttons/Breakpoint.tsx
+++ components/buttons/Breakpoint.tsx
@@ -3,9 +3,10 @@ import React from 'react';
 interface BreakpointProps {
   name: string;
   onClick: () => void;
+  icon: React.ReactNode;
 }
 
-const Breakpoint = ({ name, onClick }: BreakpointProps) => {
+const Breakpoint = ({ name, onClick, icon }: BreakpointProps) => {
   return (
     <section className="relative">
       <input
@@ -18,8 +19,9 @@ const Breakpoint = ({ name, onClick }: BreakpointProps) => {
       />
       <label
         htmlFor={name}
-        className="flex items-center text-sm px-2 py-2.5 rounded-lg border-2 border-blue-800 hover:bg-blue-800 hover:text-white peer-checked:bg-blue-800 peer-checked:text-white cursor-pointer transition"
+        className="flex items-center gap-2 text-sm p-2 rounded-lg border-2 border-blue-800 hover:bg-blue-800 hover:text-white peer-checked:bg-blue-800 peer-checked:text-white cursor-pointer transition"
       >
+        {icon}
         <p>{name}</p>
       </label>
     </section>
