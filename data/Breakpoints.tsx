024cc96a53bce47db130118f8ca2f77754eee685
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-08-19 17:33:08 -0300

    Add icon to breakpoints

---
+++ data/Breakpoints.tsx
@@ -0,0 +1,34 @@
+import DeviceDesktop from '@/components/icons/DeviceDesktop';
+import DeviceMobile from '@/components/icons/DeviceMobile';
+import DeviceMobileRotated from '@/components/icons/DeviceMobileRotated';
+import DeviceTablet from '@/components/icons/DeviceTablet';
+import DeviceTabletRotated from '@/components/icons/DeviceTabletRotated';
+import { IBreakpoint } from '@/interface/breakpoint';
+
+export const allBreakpoints: Array<IBreakpoint> = [
+  {
+    name: 'Mobile',
+    maxWidth: '340px',
+    icon: <DeviceMobile />,
+  },
+  {
+    name: 'Small',
+    maxWidth: '640px',
+    icon: <DeviceMobileRotated />,
+  },
+  {
+    name: 'Medium',
+    maxWidth: '768px',
+    icon: <DeviceTablet />,
+  },
+  {
+    name: 'Large',
+    maxWidth: '1024px',
+    icon: <DeviceTabletRotated />,
+  },
+  {
+    name: 'Full',
+    maxWidth: '100%',
+    icon: <DeviceDesktop />,
+  },
+];
