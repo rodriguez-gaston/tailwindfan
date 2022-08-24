20a690584bfcde8112bc1e18872f94529eab9247
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-08-23 23:17:43 -0300

    Display category image

--- pages/blocks/index.tsx
+++ pages/blocks/index.tsx
@@ -1,6 +1,7 @@
 import { NextPage } from 'next';
 import { blocksList } from '@/data/Blocks';
 import Link from 'next/link';
+import Image from 'next/image';
 
 const BlocksPage: NextPage = () => {
   const data = blocksList;
@@ -13,11 +14,14 @@ const BlocksPage: NextPage = () => {
 
           return (
             <Link href={`/blocks/${category.name}`} key={category.name}>
-              <a className="p-4 rounded-lg border-2 border-blue-800 hover:bg-blue-800 hover:text-white">
-                <h2 className="font-bold">{category.name}</h2>
-                <p className="text-sm">
-                  {`${blockCount} ${blockCount == 1 ? 'Block' : 'Blocks'}`}{' '}
-                </p>
+              <a className="p-4 rounded-lg border-2 border-blue-800 hover:bg-blue-800 hover:text-white flex flex-col items-center gap-2">
+                <section className="flex justify-between items-center w-full">
+                  <h2 className="font-bold">{category.name}</h2>
+                  <p className="text-sm">
+                    {`${blockCount} ${blockCount == 1 ? 'Block' : 'Blocks'}`}{' '}
+                  </p>
+                </section>
+                {category.image && <Image src={`/images/${category.image}`} width={180} height={150} />}
               </a>
             </Link>
           );
