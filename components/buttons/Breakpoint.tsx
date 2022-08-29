578e4f2c15fe9949facbc8d2b66261f5e76548c2
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-08-29 16:40:03 -0300

    Change color and styles

--- components/buttons/Breakpoint.tsx
+++ components/buttons/Breakpoint.tsx
@@ -19,7 +19,7 @@ const Breakpoint = ({ name, onClick, icon }: BreakpointProps) => {
       />
       <label
         htmlFor={name}
-        className="flex items-center gap-2 text-sm p-2 rounded-lg border-2 border-blue-800 hover:bg-blue-800 hover:text-white peer-checked:bg-blue-800 peer-checked:text-white cursor-pointer transition"
+        className="flex items-center gap-2 text-sm p-2 rounded-lg border border-gray-300 hover:bg-gray-100 peer-checked:bg-gray-100 cursor-pointer transition"
       >
         {icon}
         <p>{name}</p>
