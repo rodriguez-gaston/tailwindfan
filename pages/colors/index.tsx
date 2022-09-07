6534ed32a7ef9f2287272f766ac0ec34fd656aa9
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-09-07 17:20:27 -0300

    Include mock for color component

--- pages/colors/index.tsx
+++ pages/colors/index.tsx
@@ -1,7 +1,10 @@
+import Color from '@/components/colors/Color';
+
 const ColorsPage = () => {
   return (
     <section className="bg-gradient-to-b from-white to-cyan-50 py-20 px-8">
       <div className="max-w-7xl mx-auto">Esta es la Colors Page</div>
+      <Color />
     </section>
   );
 };
