05bd2e63b7adbcbd802db0f08dab9df471ad4e5c
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-09-08 01:04:44 -0300

    Dynamic section bar in category page

--- pages/blocks/[category].tsx
+++ pages/blocks/[category].tsx
@@ -1,9 +1,9 @@
-import React, { createRef, useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { NextPage, GetStaticProps, GetStaticPaths } from 'next';
 import { blocksList } from '@/data/Blocks';
 import Block from '@/components/blocks/Block';
 import BreakpointList from '@/components/buttons/BreakpointList';
-import Link from 'next/link';
+import SectionBar from '@/components/header/SectionBar';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const CategoryPage: NextPage = ({ category }: any) => {
@@ -11,58 +11,20 @@ const CategoryPage: NextPage = ({ category }: any) => {
   const data = blocksList;
   const categoryData = data.find((x) => x.url === propCategory.url);
 
-  const ref = createRef<HTMLElement>();
   const [viewSize, setViewSize] = useState<string>('w-full');
-  const [clientWindowHeight, setClientWindowHeight] = useState(0);
-  const [isScrolling, setIsScrolling] = useState(false);
-
-  const handleScroll = () => {
-    setClientWindowHeight(window.scrollY);
-  };
-
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  });
-
-  useEffect(() => {
-    if (ref.current?.offsetTop == clientWindowHeight) {
-      setIsScrolling(true);
-    } else {
-      setIsScrolling(false);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [clientWindowHeight]);
 
   return (
     <section className="py-10 px-8">
       <section className="max-w-7xl mx-auto space-y-12">
-        <section
-          ref={ref}
-          className={`flex items-center justify-between sticky top-0 bg-white py-4 ${
-            isScrolling && 'border-b border-gray-300'
-          }`}
+        <SectionBar
+          title={categoryData?.name ?? ''}
+          backlinkText="Back to all blocks"
+          backlinkUrl="/blocks"
         >
-          <section>
-            {isScrolling && (
-              <Link href={'/blocks'}>
-                <a className="text-gray-500 text-sm">
-                  &larr; Back to all blocks
-                </a>
-              </Link>
-            )}
-            <h1
-              className={`font-bold text-gray-800 ${
-                isScrolling ? 'text-3xl' : 'text-5xl'
-              }`}
-            >
-              {categoryData?.name}
-            </h1>
-          </section>
           <section className="lg:flex gap-2 hidden">
             <BreakpointList setViewSize={setViewSize} />
           </section>
-        </section>
+        </SectionBar>
         <section className="space-y-8">
           {categoryData?.blocks.map((block) => {
             return (
