eb4d43eaef7de7678c9119f9fe0f0d8593d6a68f
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-09-07 23:52:59 -0300

    Working on colors page

--- pages/colors/index.tsx
+++ pages/colors/index.tsx
@@ -1,10 +1,18 @@
+import React, { useState } from 'react';
 import Color from '@/components/colors/Color';
+import DirectionList from '@/components/buttons/DirectionList';
 
 const ColorsPage = () => {
+  const [gradientDirection, setGradientDirection] =
+    useState<string>('bg-gradient-to-b');
+
   return (
     <section className="bg-gradient-to-b from-white to-cyan-50 py-20 px-8">
-      <div className="max-w-7xl mx-auto">Esta es la Colors Page</div>
-      <Color />
+      <section className="max-w-7xl mx-auto">Esta es la Colors Page</section>
+      <section className="flex gap-2">
+        <DirectionList setDirection={setGradientDirection} />
+      </section>
+      <Color direction={gradientDirection} />
     </section>
   );
 };
