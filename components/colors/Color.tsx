0e7f99856797eeb2e380025e1c1e99f7bc7030ce
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-09-08 01:03:24 -0300

    Add dynamic color

--- components/colors/Color.tsx
+++ components/colors/Color.tsx
@@ -4,16 +4,17 @@ import Copy from '@/components/icons/Copy';
 import Heart from '@/components/icons/Heart';
 
 interface IColor {
-  direction?: string;
+  direction: string;
+  colors: string;
 }
 
-const Color = ({ direction = 'bg-gradient-to-b' }: IColor) => {
+const Color = ({ direction = 'bg-gradient-to-b', colors }: IColor) => {
   return (
     <section className="space-y-4">
       <section className="space-y-2">
         <h2 className="text-xl font-medium text-gray-800">Gradient</h2>
       </section>
-      <section className="border border-gray-300 rounded-lg w-full">
+      <section className="border border-gray-300 rounded-lg w-full overflow-hidden">
         <section className="border-b border-gray-300 p-2 flex items-center justify-between gap-4 rounded-t-lg text-gray-600">
           <button className="text-sm p-2 rounded-lg border border-gray-300 hover:bg-gray-100">
             <Copy />
@@ -22,9 +23,7 @@ const Color = ({ direction = 'bg-gradient-to-b' }: IColor) => {
             <Heart />
           </button>
         </section>
-        <section
-          className={`h-48 ${direction} from-blue-800 to-blue-300`}
-        ></section>
+        <section className={`h-48 ${direction} ${colors}`}></section>
       </section>
     </section>
   );
