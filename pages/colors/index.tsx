3ecc681019ae3e7282913baaa1260e848d8fcfa6
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-09-08 01:04:05 -0300

    Section bar included and map in color list

--- pages/colors/index.tsx
+++ pages/colors/index.tsx
@@ -1,18 +1,35 @@
 import React, { useState } from 'react';
 import Color from '@/components/colors/Color';
 import DirectionList from '@/components/buttons/DirectionList';
+import { colorsList } from '@/data/Colors';
+import SectionBar from '@/components/header/SectionBar';
 
 const ColorsPage = () => {
   const [gradientDirection, setGradientDirection] =
     useState<string>('bg-gradient-to-b');
 
+  const colorsData = colorsList;
+
   return (
-    <section className="bg-gradient-to-b from-white to-cyan-50 py-20 px-8">
-      <section className="max-w-7xl mx-auto">Esta es la Colors Page</section>
-      <section className="flex gap-2">
-        <DirectionList setDirection={setGradientDirection} />
+    <section className="bg-gradient-to-b from-white to-cyan-50 py-10 px-8">
+      <section className="max-w-7xl mx-auto space-y-12">
+        <SectionBar title="Colors" backlinkText="Back to Home" backlinkUrl="/">
+          <section className="flex gap-1">
+            <DirectionList setDirection={setGradientDirection} />
+          </section>
+        </SectionBar>
+        <section className="space-y-8">
+          {colorsData.map((color) => {
+            return (
+              <Color
+                key={color.name}
+                direction={gradientDirection}
+                colors={color.colors}
+              />
+            );
+          })}
+        </section>
       </section>
-      <Color direction={gradientDirection} />
     </section>
   );
 };
