a4c8d9811613f324de80a7ebf356299553aec847
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-09-08 01:05:22 -0300

    New list of colors

---
+++ data/Colors.tsx
@@ -0,0 +1,12 @@
+import { IColor } from '@/interface/color';
+
+export const colorsList: IColor[] = [
+  {
+    name: 'First',
+    colors: 'from-slate-900 to-blue-500',
+  },
+  {
+    name: 'Secton',
+    colors: 'from-blue-500 via-red-900 to-blue-900',
+  },
+];
