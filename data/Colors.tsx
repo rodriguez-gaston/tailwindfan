306f7f4445846d6381370635f9a5b4d76b9a2808
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-09-09 13:15:41 -0300

    Add ids to colors

--- data/Colors.tsx
+++ data/Colors.tsx
@@ -2,11 +2,13 @@ import { IColor } from '@/interface/color';
 
 export const colorsList: IColor[] = [
   {
+    id: '1',
     name: 'First',
     colors: 'from-slate-900 to-blue-500',
   },
   {
-    name: 'Secton',
+    id: '2',
+    name: 'Second',
     colors: 'from-blue-500 via-red-900 to-blue-900',
   },
 ];
