730044cd10f929d834bfeda20f02fc14313fdb15
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-09-09 13:16:26 -0300

    Include new props

--- pages/colors/index.tsx
+++ pages/colors/index.tsx
@@ -18,13 +18,15 @@ const ColorsPage = () => {
             <DirectionList setDirection={setGradientDirection} />
           </section>
         </SectionBar>
-        <section className="space-y-8">
+        <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
           {colorsData.map((color) => {
             return (
               <Color
-                key={color.name}
+                key={color.id}
                 direction={gradientDirection}
                 colors={color.colors}
+                name={color.name}
+                id={color.id}
               />
             );
           })}
