eb94dd3250340d09ba662e5aa87413e099762047
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-09-09 13:15:54 -0300

    Add id

--- interface/color.ts
+++ interface/color.ts
@@ -1,4 +1,16 @@
 export interface IColor {
+  id: string;
   name: string;
   colors: string;
+}
+
+export interface ColorProps {
+  direction: string;
+  colors: string;
+  name: string;
+  id: string;
+}
+
+export interface FavColor {
+  id: string;
 }
