e5d26f60a9163904340d5f972ab5d9c4014bf53b
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-09-10 20:56:38 -0300

    Change color

--- components/buttons/FavButton.tsx
+++ components/buttons/FavButton.tsx
@@ -10,7 +10,7 @@ const FavButton = ({ isFav, onClick }: Props) => {
   return (
     <button
       className={`text-sm p-2 rounded-lg border border-gray-300 hover:bg-gray-100 ${
-        isFav && 'bg-red-200 border-red-200 text-white hover:text-gray-600'
+        isFav && 'bg-red-500 border-red-500 text-white hover:text-gray-600'
       }`}
       onClick={onClick}
     >
