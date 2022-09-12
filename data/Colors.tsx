1aa8428be20e55dd5977e8e1005d4914f970a5a1
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-09-12 16:57:22 -0300

    Include new colors

--- data/Colors.tsx
+++ data/Colors.tsx
@@ -3,12 +3,86 @@ import { IColor } from '@/interface/color';
 export const colorsList: IColor[] = [
   {
     id: '1',
-    name: 'First',
-    colors: 'from-slate-900 to-blue-500',
+    colors: 'from-[#0061ff] to-[#60efff]',
   },
   {
     id: '2',
-    name: 'Second',
-    colors: 'from-blue-500 via-red-900 to-blue-900',
+    colors: 'from-[#a9ff68] to-[#ff8989]',
+  },
+  {
+    id: '3',
+    colors: 'from-[#ff416c] to-[#ff4b2b]',
+  },
+  {
+    id: '4',
+    colors: 'from-[#a8ff78] to-[#78ffd6]',
+  },
+  {
+    id: '5',
+    colors: 'from-[#ffa6b7] to-[#1e2ad2]',
+  },
+  {
+    id: '6',
+    colors: 'from-[#f40076] to-[#df98fA]',
+  },
+  {
+    id: '7',
+    colors: 'from-[#f06966] to-[#fad6a6]',
+  },
+  {
+    id: '8',
+    colors: 'from-[#ff0076] to-[#590fb7]',
+  },
+  {
+    id: '9',
+    colors: 'from-[#9055ff] to-[#13e2da]',
+  },
+  {
+    id: '10',
+    colors: 'from-[#d6ff7f] to-[#00b3cc]',
+  },
+  {
+    id: '11',
+    colors: 'from-[#e233ff] to-[#ff6b00]',
+  },
+  {
+    id: '12',
+    colors: 'from-[#fa8bff] via-[#2bd2ff] to-[#2bff88]',
+  },
+  {
+    id: '13',
+    colors: 'from-[#fceaa3] via-[#fe8676] to-[#bd57a7]',
+  },
+  {
+    id: '14',
+    colors: 'from-[#c6ffdd] via-[#f8d786] to-[#f7797d]',
+  },
+  {
+    id: '15',
+    colors: 'from-red-400 to-red-800',
+  },
+  {
+    id: '16',
+    colors: 'from-pink-400 to-yellow-400',
+  },
+  {
+    id: '17',
+    colors: 'from-gray-500 to-gray-900',
+  },
+  {
+    id: '18',
+    colors: 'from-indigo-300 via-red-300 to-yellow-200',
+  },
+  {
+    id: '19',
+    colors: 'from-blue-700 via-blue-800 to-gray-900',
+  },
+  {
+    id: '20',
+    colors: 'from-rose-400 via-fuchsia-400 to-indigo-400',
+  },
+  {
+    id: '21',
+    colors: 'from-violet-500 to-orange-300',
   },
 ];
