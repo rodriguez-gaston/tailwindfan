45228599f020fdbe7ecaa37779a042345a4e436c
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-09-19 11:52:51 -0300

    New customs gradients

--- data/Colors.tsx
+++ data/Colors.tsx
@@ -3,86 +3,242 @@ import { IColor } from '@/interface/color';
 export const colorsList: IColor[] = [
   {
     id: '1',
-    colors: 'from-[#0061ff] to-[#60efff]',
-  },
-  {
-    id: '2',
-    colors: 'from-[#a9ff68] to-[#ff8989]',
-  },
-  {
-    id: '3',
-    colors: 'from-[#ff416c] to-[#ff4b2b]',
-  },
-  {
-    id: '4',
-    colors: 'from-[#a8ff78] to-[#78ffd6]',
-  },
-  {
-    id: '5',
-    colors: 'from-[#ffa6b7] to-[#1e2ad2]',
-  },
-  {
-    id: '6',
-    colors: 'from-[#f40076] to-[#df98fA]',
-  },
-  {
-    id: '7',
-    colors: 'from-[#f06966] to-[#fad6a6]',
-  },
-  {
-    id: '8',
-    colors: 'from-[#ff0076] to-[#590fb7]',
-  },
-  {
-    id: '9',
-    colors: 'from-[#9055ff] to-[#13e2da]',
-  },
-  {
-    id: '10',
-    colors: 'from-[#d6ff7f] to-[#00b3cc]',
-  },
-  {
-    id: '11',
-    colors: 'from-[#e233ff] to-[#ff6b00]',
-  },
-  {
-    id: '12',
-    colors: 'from-[#fa8bff] via-[#2bd2ff] to-[#2bff88]',
-  },
-  {
-    id: '13',
-    colors: 'from-[#fceaa3] via-[#fe8676] to-[#bd57a7]',
-  },
-  {
-    id: '14',
-    colors: 'from-[#c6ffdd] via-[#f8d786] to-[#f7797d]',
-  },
-  {
-    id: '15',
     colors: 'from-red-400 to-red-800',
   },
   {
-    id: '16',
+    id: '2',
     colors: 'from-pink-400 to-yellow-400',
   },
   {
-    id: '17',
+    id: '3',
     colors: 'from-gray-500 to-gray-900',
   },
   {
-    id: '18',
+    id: '4',
     colors: 'from-indigo-300 via-red-300 to-yellow-200',
   },
   {
-    id: '19',
+    id: '5',
     colors: 'from-blue-700 via-blue-800 to-gray-900',
   },
   {
-    id: '20',
+    id: '6',
     colors: 'from-rose-400 via-fuchsia-400 to-indigo-400',
   },
   {
-    id: '21',
+    id: '7',
     colors: 'from-violet-500 to-orange-300',
+  },
+  {
+    id: '8',
+    colors: 'from-[#0061ff] to-[#60efff]',
+  },
+  {
+    id: '9',
+    colors: 'from-[#a9ff68] to-[#ff8989]',
+  },
+  {
+    id: '10',
+    colors: 'from-[#ff416c] to-[#ff4b2b]',
+  },
+  {
+    id: '11',
+    colors: 'from-[#a8ff78] to-[#78ffd6]',
+  },
+  {
+    id: '12',
+    colors: 'from-[#ffa6b7] to-[#1e2ad2]',
+  },
+  {
+    id: '13',
+    colors: 'from-[#f40076] to-[#df98fA]',
+  },
+  {
+    id: '14',
+    colors: 'from-[#f06966] to-[#fad6a6]',
+  },
+  {
+    id: '15',
+    colors: 'from-[#ff0076] to-[#590fb7]',
+  },
+  {
+    id: '16',
+    colors: 'from-[#9055ff] to-[#13e2da]',
+  },
+  {
+    id: '17',
+    colors: 'from-[#d6ff7f] to-[#00b3cc]',
+  },
+  {
+    id: '18',
+    colors: 'from-[#e233ff] to-[#ff6b00]',
+  },
+  {
+    id: '19',
+    colors: 'from-[#ff0a45] to-[#ffaf98]',
+  },
+  {
+    id: '20',
+    colors: 'from-[#e0c4fd] to-[#8fc6fd]',
+  },
+  {
+    id: '21',
+    colors: 'from-[#dbe2f7] to-[#d7a6a6]',
+  },
+  {
+    id: '22',
+    colors: 'from-[#fdfbfb] to-[#eceeef]',
+  },
+  {
+    id: '23',
+    colors: 'from-[#ffe25a] to-[#ffa852]',
+  },
+  {
+    id: '24',
+    colors: 'from-[#2dc1e4] to-[#e8ecc7]',
+  },
+  {
+    id: '25',
+    colors: 'from-[#f7f8f7] to-[#aebc7a]',
+  },
+  {
+    id: '26',
+    colors: 'from-[#df6363] to-[#ffb88c]',
+  },
+  {
+    id: '27',
+    colors: 'from-[#ff9ca0] to-[#ffcfef]',
+  },
+  {
+    id: '28',
+    colors: 'from-[#a38ed2] to-[#f9c1eb]',
+  },
+  {
+    id: '29',
+    colors: 'from-[#ffecd2] to-[#fdb8a1]',
+  },
+  {
+    id: '30',
+    colors: 'from-[#fdccf1] to-[#f8edfb]',
+  },
+  {
+    id: '31',
+    colors: 'from-[#a2c5fe] to-[#c2e9fc]',
+  },
+  {
+    id: '32',
+    colors: 'from-[#d0dae0] to-[#e2ebf0]',
+  },
+  {
+    id: '33',
+    colors: 'from-[#adccef] to-[#e7f0fd]',
+  },
+  {
+    id: '34',
+    colors: 'from-[#ef9da8] to-[#ffdce0]',
+  },
+  {
+    id: '35',
+    colors: 'from-[#2baaff] to-[#26479c]',
+  },
+  {
+    id: '36',
+    colors: 'from-[#ce9ef3] to-[#f6f3ff]',
+  },
+  {
+    id: '37',
+    colors: 'from-[#c4f4f2] to-[#69ebfc]',
+  },
+  {
+    id: '38',
+    colors: 'from-[#8383be] to-[#50a7d9]',
+  },
+  {
+    id: '39',
+    colors: 'from-[#e0eafd] to-[#b2cbee]',
+  },
+  {
+    id: '40',
+    colors: 'from-[#7bdfe6] to-[#5894a6]',
+  },
+  {
+    id: '41',
+    colors: 'from-[#7d7be6] to-[#9558a6]',
+  },
+  {
+    id: '42',
+    colors: 'from-[#cac9ee] to-[#e9c4f3]',
+  },
+  {
+    id: '43',
+    colors: 'from-[#9896f0] to-[#fbc8d5]',
+  },
+  {
+    id: '44',
+    colors: 'from-[#71c5ee] to-[#025091]',
+  },
+  {
+    id: '45',
+    colors: 'from-[#fbee97] to-[#adedd5]',
+  },
+  {
+    id: '46',
+    colors: 'from-[#ff7082] to-[#c0e9ff]',
+  },
+  {
+    id: '47',
+    colors: 'from-[#bcc5ce] to-[#939fae]',
+  },
+  {
+    id: '48',
+    colors: 'from-[#fdfcfb] to-[#e3d2c4]',
+  },
+  {
+    id: '49',
+    colors: 'from-[#626262] to-[#9bc5c3]',
+  },
+  {
+    id: '50',
+    colors: 'from-[#dbd5ed] to-[#f3e7ea]',
+  },
+  {
+    id: '51',
+    colors: 'from-[#a18cd1] to-[#fbc2eb]',
+  },
+  {
+    id: '52',
+    colors: 'from-[#d4fc79] to-[#96e6a1]',
+  },
+  {
+    id: '53',
+    colors: 'from-[#cd9cf2] to-[#e2d9ff]',
+  },
+  {
+    id: '54',
+    colors: 'from-[#ffcf9f] to-[#ff889c]',
+  },
+  {
+    id: '55',
+    colors: 'from-[#64b3f4] to-[#c2e59c]',
+  },
+  {
+    id: '56',
+    colors: 'from-[#7028e4] to-[#e5b2ca]',
+  },
+  {
+    id: '57',
+    colors: 'from-[#c6d0d6] to-[#e2ebf0]',
+  },
+  {
+    id: '58',
+    colors: 'from-[#fa8bff] via-[#2bd2ff] to-[#2bff88]',
+  },
+  {
+    id: '59',
+    colors: 'from-[#fceaa3] via-[#fe8676] to-[#bd57a7]',
+  },
+  {
+    id: '60',
+    colors: 'from-[#c6ffdd] via-[#f8d786] to-[#f7797d]',
   },
 ];
