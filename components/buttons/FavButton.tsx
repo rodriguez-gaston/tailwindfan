9b4d40fb44da5c7e598c4922621dcb247d6d20e2
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-09-22 16:00:13 -0300

    Include alt to button

--- components/buttons/FavButton.tsx
+++ components/buttons/FavButton.tsx
@@ -14,6 +14,7 @@ const FavButton = ({ isFav, onClick }: Props) => {
       }`}
       onClick={onClick}
     >
+      <span className="sr-only">Add to favorites</span>
       <Heart />
     </button>
   );
