b3af845c4bbdcfd4e9d7f6dd066008d4c6b36847
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-09-26 13:23:13 -0300

    Include google analytics script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,29 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import DefaultLayout from '@/layouts/DefaultLayout';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <DefaultLayout>
-      <Component {...pageProps} />
-    </DefaultLayout>
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      ></Script>
+      <Script strategy="lazyOnload" id="google-script">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            page_path: window.location.pathname,
+            });
+        `}
+      </Script>
+      <DefaultLayout>
+        <Component {...pageProps} />
+      </DefaultLayout>
+    </>
   );
 }
 
