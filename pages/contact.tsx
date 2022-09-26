59747fe8d7cbacaeef5a7697202b3caec9353d0b
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-09-26 13:06:49 -0300

    Delete contact page

--- pages/contact.tsx
+++
@@ -1,9 +0,0 @@
-const ContactPage = () => {
-  return (
-    <section className="bg-gradient-to-b from-white to-cyan-50 py-20 px-8">
-      <div className="max-w-7xl mx-auto">Esta es la Contact Page</div>
-    </section>
-  );
-};
-
-export default ContactPage;
