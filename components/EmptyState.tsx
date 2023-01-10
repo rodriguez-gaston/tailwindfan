acb39fd8505a6076fa22c02d1face074a61732a9
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2023-01-10 11:09:02 -0300

    Add legacyBehavior

--- components/EmptyState.tsx
+++ components/EmptyState.tsx
@@ -10,8 +10,8 @@ interface Props {
 
 const EmptyState = ({ image, text, url }: Props) => {
   return (
-    <section className="w-full mx-auto flex flex-col items-center gap-4">
-      <h5 className="text-gray-800 font-medium text-lg">{`You didn't add any ${text.toLowerCase()}`}</h5>
+    <section className="flex flex-col items-center w-full gap-4 mx-auto">
+      <h5 className="text-lg font-medium text-gray-800">{`You didn't add any ${text.toLowerCase()}`}</h5>
       <Image
         src={image}
         alt={`No ${text}s added`}
@@ -19,8 +19,8 @@ const EmptyState = ({ image, text, url }: Props) => {
         width={150}
         height={150}
       />
-      <Link href={url} passHref>
-        <a className="border-2 border-blue-700 hover:border-blue-900 bg-blue-700 hover:bg-blue-900 transition duration-300 text-white rounded-lg px-4 py-3">{`View all ${text}s`}</a>
+      <Link href={url} passHref legacyBehavior>
+        <a className="px-4 py-3 text-white transition duration-300 bg-blue-700 border-2 border-blue-700 rounded-lg hover:border-blue-900 hover:bg-blue-900">{`View all ${text}s`}</a>
       </Link>
     </section>
   );
